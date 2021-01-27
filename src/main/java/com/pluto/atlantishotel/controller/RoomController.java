57ee2a35ec8f9f724bd03e9eddb11acaea6dd29b
AESHAH <aisha379aisha@gmail.com>
2021-01-27 13:30:39 +0300

    done room controller and model and Dao  without testing and jsp

---
+++ src/main/java/com/pluto/atlantishotel/controller/RoomController.java
@@ -0,0 +1,62 @@
+package com.pluto.atlantishotel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.pluto.atlantishotel.dao.RoomDao;
+import com.pluto.atlantishotel.model.Room;
+
+@Controller
+public class RoomController {
+
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private RoomDao dao;
+	
+	  @GetMapping("/room/?")
+	   public ModelAndView room() {
+		   
+		   ModelAndView mv = new ModelAndView();
+//		   mv.setViewName("room/?");
+		   
+		   HomeController hc = new HomeController();
+		   hc.setAppName(mv, env);
+		   
+		   return mv;
+	   }
+	  
+	  @PostMapping("/room/?")
+		 public String registration(Room room) {
+
+			 
+			 ModelAndView mv = new ModelAndView();
+//			 mv.setViewName("room/?");
+			 
+			 HomeController hc = new HomeController();
+			 hc.setAppName(mv, env);
+			 
+			 // Check to room is already book or not
+			 
+			 var it = dao.findAll();
+			 
+			 
+			 for(Room dbRoom : it)
+			 {
+				 if( dbRoom.getId()== dbRoom.getId() ) {
+					 mv.addObject("message", "Sorry,Room already booked");
+					 return "redirect:/";
+				 }
+			 }
+			 dao.save(room);
+			 mv.addObject("message", "your booking Accepted");
+			 
+			 return "redirect:/";	 
+	  }
+}
---
+++ src/main/java/com/pluto/atlantishotel/model/Room.java
@@ -0,0 +1,66 @@
+package com.pluto.atlantishotel.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Room")
+public class Room {
+
+	
+	 @Id
+	 private int id;
+	
+	 private int price;
+	
+	 private String roomType;
+	 
+	 private String image;
+	 
+	 private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	 
+	 
+
+	
+}
